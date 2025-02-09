22c177e824efddafab9df76f2f7e2227ec15885b
fogInSingularity <vladimirnaumov979@gmail.com>
2025-02-09 11:41:56 +0300

    add spdlog as logger

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,10 @@
 #include <fstream>
 #include <iostream>
 
+#include <spdlog/common.h>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+
 #include "ast.hpp"
 #include "driver.hpp"
 #include "interpreter.hpp"
@@ -9,7 +13,24 @@
 int yyFlexLexer::yywrap() { return 1; }
     
 int main(const int argc, const char* argv[]) {
+    auto logger = spdlog::basic_logger_mt("paracl", "../paracl.log", /*override log?*/ true);
+    spdlog::set_default_logger(logger);
+
+#if defined (NDEBUG)
+    spdlog::set_level(spdlog::level::info);
+#else // NDEBUG
+    // spdlog::flush_on(spdlog::level::trace);
+    spdlog::set_level(spdlog::level::debug);
+#endif // NDEBUG
+
+    // log argv
+    for (int i = 0; i < argc; i++) {
+        spdlog::info("argv[{}]: {}", i, argv[i]);
+    }
+
     if (argc < 2) {
+        spdlog::error("not enough files");
+
         std::cerr << "Not enough files\n";
         std::cerr << "Usage: " << argv[0] << " <src_file>\n";
 
@@ -18,6 +39,8 @@ int main(const int argc, const char* argv[]) {
 
     std::ifstream pcl_src_file(argv[1]);
     if (!pcl_src_file) {
+        spdlog::error("could not open file {}", argv[1]);
+
         std::cerr << "Error: could not open file: " << argv[1] << "\n";
         
         return EXIT_FAILURE;
